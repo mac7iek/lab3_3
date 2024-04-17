cf768e9dda9190fc4099b1fbfa3177298f828f6c
mac7iek <161622341+mac7iek@users.noreply.github.com>
2024-04-17 15:03:04 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Podaj znak, którym chcesz stworzyć własny trójkąt: ");
+    char symbol = scanner.next().charAt(0);
+
+    System.out.println("podaj wysokość tego trójkąta: ");
+    int height = scanner.nextInt();
+    for(int i = 1; i <= height; i++) {
+      for(int j = 1; j <= i; j++) {
+        System.out.print(symbol + " ");
+      }
+       System.out.println();
+    }
+  }
+}
